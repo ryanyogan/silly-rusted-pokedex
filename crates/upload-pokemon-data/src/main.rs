6626e34b86a709f38d042eef033a1dda373fdb16
Ryan Yogan <ryanyogan@gmail.com>
2023-12-19 17:47:46 -0600

    feat // getting ready to sqeeel

---
+++ crates/upload-pokemon-data/src/main.rs
@@ -0,0 +1,16 @@
+mod db;
+mod pokemon_csv;
+use db::*;
+use pokemon_csv::*;
+
+fn main() -> Result<(), csv::Error> {
+    let mut rdr = csv::Reader::from_path("./crates/upload-pokemon-data/pokemon.csv")?;
+
+    for result in rdr.deserialize::<PokemonCsv>() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    dbg!(PokemonId::new());
+    Ok(())
+}
