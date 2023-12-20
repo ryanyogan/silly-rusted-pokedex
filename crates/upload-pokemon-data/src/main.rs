b3c113a1b22cc3eb7a65cc9ab2a32774923f9678
Ryan Yogan <ryanyogan@gmail.com>
2023-12-19 18:55:44 -0600

    feat // the script works ;)

--- crates/upload-pokemon-data/src/main.rs
+++ crates/upload-pokemon-data/src/main.rs
@@ -1,15 +1,39 @@
 mod db;
 mod pokemon_csv;
+use std::env;
+
+use color_eyre::{
+    eyre::{Context, Result},
+    Section,
+};
 use db::*;
 use pokemon_csv::*;
+use sqlx::mysql::MySqlPoolOptions;
 
-fn main() -> Result<(), csv::Error> {
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let database_url = env::var("DATABASE_URL")
+        .wrap_err("Must have a DATABASE_URL set")
+        .suggestion("Run `pscale connect <database> <branch>` to get a DATABASE_URL")?;
+
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+
     let mut rdr = csv::Reader::from_path("./crates/upload-pokemon-data/pokemon.csv")?;
 
     for result in rdr.deserialize::<PokemonCsv>() {
         let record = result?;
         let pokemon_row: PokemonTableRow = record.into();
-        println!("{:?}", pokemon_row);
+        println!(
+            "{} {:?} {}",
+            pokemon_row.pokedex_id, pokemon_row.id, pokemon_row.name
+        );
+
+        insert_pokemon(&pool, &pokemon_row).await?;
     }
 
     Ok(())
